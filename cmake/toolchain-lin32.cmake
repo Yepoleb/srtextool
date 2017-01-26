955e79ba4e4da9bd942c22909b97d2a0bc1e225d
Gabriel Huber <huberg18@gmail.com>
2017-01-26 02:29:04 +0100

    Consistent style for toolchains

--- cmake/toolchain-lin32.cmake
+++ cmake/toolchain-lin32.cmake
@@ -2,20 +2,20 @@
 # You don't need it when building on the target system
 
 # the name of the target operating system
-SET(CMAKE_SYSTEM_NAME Linux)
+set (CMAKE_SYSTEM_NAME Linux)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER gcc)
-SET(CMAKE_CXX_COMPILER g++)
+set (CMAKE_C_COMPILER gcc)
+set (CMAKE_CXX_COMPILER g++)
 
 # set 32bit flags
-SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
-SET(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
-SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
+set (CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
+set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "" FORCE)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
 # programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set (CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set (CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
